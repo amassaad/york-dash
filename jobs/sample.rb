e237d3cbf8a459abd33822182718f822b4d479d4
amassaad <alex.massaad@jadedpixel.com>
2014-03-01 15:55:22 -0500

    Updating repo

--- jobs/sample.rb
+++ jobs/sample.rb
@@ -1,7 +1,7 @@
 current_valuation = 0
 current_karma = 0
 
-SCHEDULER.every '2s' do
+SCHEDULER.every '10s' do
   last_valuation = current_valuation
   last_karma     = current_karma
   current_valuation = rand(100)
@@ -9,5 +9,5 @@ SCHEDULER.every '2s' do
 
   send_event('valuation', { current: current_valuation, last: last_valuation })
   send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
+  send_event('synergy',   { value: rand(1000) })
 end
--- widgets/oc_transpo/oc_transpo.coffee
+++
@@ -1,1 +0,0 @@
-class Dashing.OcTranspo extends Dashing.Widget
