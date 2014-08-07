0138061a0436e9674d73f958f463f3eada181774
Alex Massaad <alex.massaad@jadedpixel.com>
2014-08-07 18:39:35 -0400

    Push kittens

--- jobs/iframe.rb
+++ jobs/iframe.rb
@@ -1,5 +1,5 @@
 SCHEDULER.every '20s', :first_in => 0 do |job|
-    url = ""
+    url = "http://33.media.tumblr.com/c258355d21dc46c2eba7e17177a0157c/tumblr_n2v0753m531rudcwro1_500.gif"
     puts url
     send_event('iframe2', url: url)
 end
