f7a12b014ccc8547b358029d81d5ddb94998f779
Alex Massaad <alex.massaad@jadedpixel.com>
2014-08-10 18:27:27 -0400

    standing cats

--- jobs/random_cats.rb
+++ jobs/random_cats.rb
@@ -5,7 +5,7 @@ placeholder = '/assets/nyantocat.gif'
 
 SCHEDULER.every '75s', first_in: 0 do |job|
   http = Net::HTTP.new('www.reddit.com')
-  response = http.request(Net::HTTP::Get.new("/r/catgifs.json"))
+  response = http.request(Net::HTTP::Get.new("/r/catsstandingup.json"))
   json = JSON.parse(response.body)
 
   if json['data']['children'].count <= 0
@@ -14,7 +14,7 @@ SCHEDULER.every '75s', first_in: 0 do |job|
     urls = json['data']['children'].map{|child| child['data']['url'] }
 
     # Ensure we're linking directly to an image, not a gallery etc.
-    valid_urls = urls.select{|url| url.downcase.end_with?('gif')}
+    valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
     send_event('cats', image: "background-image:url(#{valid_urls.sample(1).first})")
   end
 end
