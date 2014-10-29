44dfab616345a134a5f24b99739800b7595311de
amassaad <alex.massaad@shopify.com>
2014-10-29 19:58:39 -0400

    Hotfix for outage

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -1,4 +1,3 @@
 SCHEDULER.every '15s', first_in: 0 do |job|
-    send_event('gallery', image: "background-image:url(http://media.gallery.ca/newcam.jpg)")
-  end
+  send_event('gallery', image: "background-image:url(http://media.gallery.ca/newcam.jpg)")
 end
