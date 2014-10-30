b8fb93003b5009bd394ffb585755212951adeebb
amassaad <alex.massaad@shopify.com>
2014-10-29 22:06:49 -0400

    fscking images

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -1,10 +1,12 @@
 require 'net/http'
 require 'json'
 
-SCHEDULER.every '28s', first_in: 0 do |job|
+SCHEDULER.every '14s', first_in: 0 do |job|
   url = "http://media.gallery.ca/newcam.jpg"
   fake_url = "http://media.gallery.ca/newmac.jpg"
   send_event('gallery', image: "background-image:url(#{fake_url})")
-  sleep(1)
+  sleep(1.0/2.0)
+  send_event('gallery', image: "background-image:url(#{url})")  
+  sleep(1.0/2.0)
   send_event('gallery', image: "background-image:url(#{url})")
 end
