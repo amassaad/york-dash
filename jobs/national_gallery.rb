77b60e89a1a5ed38e50190c4436c5d479bf2c849
amassaad <alex.massaad@shopify.com>
2014-10-29 21:07:12 -0400

    Fixing missig parts, fixing nat gal cam

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -1,3 +1,27 @@
-SCHEDULER.every '15s', first_in: 0 do |job|
-  send_event('gallery', image: "background-image:url(http://media.gallery.ca/newcam.jpg)")
+require 'net/http'
+require 'json'
+
+SCHEDULER.every '30s', first_in: 0 do |job|
+
+  url = "http://media.gallery.ca/newcam.jpg"
+  # resp = Net::HTTP.get_response(URI.parse(url))
+  # jurl = JSON.parse(url)
+
+  send_event('gallery', image: "background-image:url(#{url})")
 end
+
+# SCHEDULER.every '60s', first_in: 0 do |job|
+#   http = Net::HTTP.new('www.reddit.com')
+#   response = http.request(Net::HTTP::Get.new("/r/cats/top.json"))
+#   json = JSON.parse(response.body)
+
+#   if json['data']['children'].count <= 0
+#     send_event('aww', image: placeholder)
+#   else
+#     urls = json['data']['children'].map{|child| child['data']['url'] }
+
+#     # Ensure we're linking directly to an image, not a gallery etc.
+#     valid_urls = urls.select{|url| url.downcase.end_with?('png', 'gif', 'jpg', 'jpeg')}
+#     send_event('aww', image: "background-image:url(#{valid_urls.sample(1).first})")
+#   end
+# end
