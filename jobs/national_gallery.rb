840cb1471fdf523ebf2093a8f56c0029e0976013
amassaad <alex.massaad@shopify.com>
2014-10-29 21:41:56 -0400

    Fix update of gallery cam

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -1,9 +1,9 @@
 require 'net/http'
 require 'json'
 
-SCHEDULER.every '10s', first_in: 0 do |job|
+SCHEDULER.every '28s', first_in: 0 do |job|
   url = "http://media.gallery.ca/newcam.jpg"
-  fake_url "http://media.gallery.ca/newmac.jpg"
+  fake_url = "http://media.gallery.ca/newmac.jpg"
   send_event('gallery', image: "background-image:url(#{fake_url})")
   sleep(1)
   send_event('gallery', image: "background-image:url(#{url})")
