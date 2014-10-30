f224b1db4eb3d9b84e5354c38b83e2117225fe0b
amassaad <alex.massaad@shopify.com>
2014-10-29 21:33:54 -0400

    Faker

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -3,5 +3,8 @@ require 'json'
 
 SCHEDULER.every '10s', first_in: 0 do |job|
   url = "http://media.gallery.ca/newcam.jpg"
+  fake_url "http://media.gallery.ca/newmac.jpg"
+  send_event('gallery', image: "background-image:url(#{fake_url})")
+  sleep(1)
   send_event('gallery', image: "background-image:url(#{url})")
 end
