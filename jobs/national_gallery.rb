9479c9a3dfd577ec0c8587360c3b6bb8af66e473
amassaad <alex.massaad@shopify.com>
2014-11-13 16:16:56 -0500

    Update stuff

--- jobs/national_gallery.rb
+++ jobs/national_gallery.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'json'
 
-SCHEDULER.every '29s', first_in: 0 do |job|
+SCHEDULER.every '19s', first_in: 0 do |job|
   url = "http://media.gallery.ca/newcam.jpg"
   fake_url = "http://media.gallery.ca/newmac.jpg"
   send_event('gallery', image: "background-image:url(#{fake_url})")
