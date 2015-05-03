325b5bcf0ea23f7809f6cc8149d55009d48e45ed
amassaad <massaad@gmail.com>
2015-05-03 11:01:55 -0400

    Update different working webcams for the widget

--- jobs/octranspo.rb
+++ jobs/octranspo.rb
@@ -1,47 +1,47 @@
-require 'octranspo_fetch'
+# require 'octranspo_fetch'
  
-APPLICATION_ID = ENV['OC_ID']
-APPLICATION_KEY = ENV['OC_KEY']
-MAX_RESULTS = 4
-STOPS = {
-    "3009" => {description: "Rideau Street", buses: [1,95,97]},
-    "3000" => {description: "Rideau Center (bridge)", buses: [8, 95, 97]}
-}
+# APPLICATION_ID = ENV['OC_ID']
+# APPLICATION_KEY = ENV['OC_KEY']
+# MAX_RESULTS = 4
+# STOPS = {
+#     "3009" => {description: "Rideau Street", buses: [1,95,97]},
+#     "3000" => {description: "Rideau Center (bridge)", buses: [8, 95, 97]}
+# }
  
  
-oct = OCTranspo.new({application_id: APPLICATION_ID, application_key: APPLICATION_KEY})
+# oct = OCTranspo.new({application_id: APPLICATION_ID, application_key: APPLICATION_KEY})
  
-SCHEDULER.every '2m', :first_in => 0 do
-    # Get data for each stop
-    no_data = []
-    trips = []
-    STOPS.each do |stop_no, stop_info|
-        route_nos = stop_info[:buses]
-        trips_for_stop = oct.simple_get_next_trips_for_stop stop_no, route_nos
-        if trips_for_stop.length == 0
-            no_data.push (stop_info[:description] or stop_no)
-        else
-            trips_for_stop.each do |trip|
-                if stop_info[:description]
-                    trip[:stop_description] = stop_info[:description]
-                end
-            end
-            trips.concat trips_for_stop
-        end
-    end
+# SCHEDULER.every '2m', :first_in => 0 do
+#     # Get data for each stop
+#     no_data = []
+#     trips = []
+#     STOPS.each do |stop_no, stop_info|
+#         route_nos = stop_info[:buses]
+#         trips_for_stop = oct.simple_get_next_trips_for_stop stop_no, route_nos
+#         if trips_for_stop.length == 0
+#             no_data.push (stop_info[:description] or stop_no)
+#         else
+#             trips_for_stop.each do |trip|
+#                 if stop_info[:description]
+#                     trip[:stop_description] = stop_info[:description]
+#                 end
+#             end
+#             trips.concat trips_for_stop
+#         end
+#     end
  
-    # Sort everything by arrival time.
-    trips.sort! { |a,b| a[:arrival_in_minutes] <=> b[:arrival_in_minutes] }
+#     # Sort everything by arrival time.
+#     trips.sort! { |a,b| a[:arrival_in_minutes] <=> b[:arrival_in_minutes] }
  
-    # Limit number of trips we return
-    trips = trips[0...MAX_RESULTS]
+#     # Limit number of trips we return
+#     trips = trips[0...MAX_RESULTS]
  
-    moreinfo = if no_data.length > 0
-        "No data for stops: #{no_data.join ", "}"
-    else
-        cache_stats = oct.cache_stats()
-        "Cache: #{cache_stats[:hits]} hits, #{cache_stats[:misses]} misses"
-    end
+#     moreinfo = if no_data.length > 0
+#         "No data for stops: #{no_data.join ", "}"
+#     else
+#         cache_stats = oct.cache_stats()
+#         "Cache: #{cache_stats[:hits]} hits, #{cache_stats[:misses]} misses"
+#     end
  
-    send_event('octranspo', { trips: trips, moreinfo: moreinfo})
-end
+#     send_event('octranspo', { trips: trips, moreinfo: moreinfo})
+# end
--- widgets/webcam/webcam.coffee
+++ widgets/webcam/webcam.coffee
@@ -1,4 +1,4 @@
-class Dashing.NationalGallery extends Dashing.Widget
+class Dashing.Webcam extends Dashing.Widget
 
   ready: ->
     # This is fired when the widget is done being rendered
