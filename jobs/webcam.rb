65701ca4fed16437d651ad6e5821f2ea9877a06c
amassaad <massaad@gmail.com>
2015-06-28 14:32:17 -0400

    Update dash

--- jobs/webcam.rb
+++ jobs/webcam.rb
@@ -1,23 +1,40 @@
 require 'net/http'
 require 'json'
 
-SCHEDULER.every '15s', first_in: 0 do |job|
+fake_url = "http://traffic.ottawa.ca"
+#I am the local cam (St Patrick and Dalhousie)
+url1 = "http://traffic.ottawa.ca/map/camera?id=185"
+#I am the War Museum cam (Booth and Sir John A. Macdonald Parkway)
+url2 = "http://traffic.ottawa.ca/map/camera?id=49"
+#I am a camera near work
+url3 = "http://traffic.ottawa.ca/map/camera?id=35"
+#I am the camera at Rideau and Sussex
+url4 = "http://traffic.ottawa.ca/map/camera?id=33"
 
-  #I am the local cam (St Patrick and Dalhousie)
-  url1 = "http://traffic.ottawa.ca/map/camera?id=185"
-  #I am the War Museum cam (Booth and Sir John A. Macdonald Parkway)
-  url2 = "http://traffic.ottawa.ca/map/camera?id=49"
+
+SCHEDULER.every '15s', first_in: 0 do |job|
   #We need to trick the browser into loading an erronious fake URL first to create the low fps "video" effect.
-  fake_url = "http://traffic.ottawa.ca/map/"
   send_event('cam1', image: "background-image:url(#{fake_url})")
   sleep(1.0/30.0)
   send_event('cam2', image: "background-image:url(#{fake_url})")
   sleep(1.0/30.0)
+  send_event('cam3', image: "background-image:url(#{fake_url})")
+  sleep(1.0/30.0)
+  send_event('cam4', image: "background-image:url(#{fake_url})")
+  sleep(1.0/30.0)
   send_event('cam1', image: "background-image:url(#{url1})")
   sleep(1.0/30.0)
   send_event('cam2', image: "background-image:url(#{url2})")
+  sleep(1.0/30.0)
+  send_event('cam3', image: "background-image:url(#{url3})")
+  sleep(1.0/30.0)
+  send_event('cam4', image: "background-image:url(#{url4})")
   sleep(1.0/2.0)
   send_event('cam1', image: "background-image:url(#{url1})")
-  sleep(1.0/30.0)
+  sleep(1.0/2.0)
   send_event('cam2', image: "background-image:url(#{url2})")
+  sleep(1.0/2.0)
+  send_event('cam3', image: "background-image:url(#{url3})")
+  sleep(1.0/2.0)
+  send_event('cam4', image: "background-image:url(#{url4})")
 end
