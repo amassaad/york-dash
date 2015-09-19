0a8014ea1341d05c657c2dd3465d0391de393892
amassaad <massaad@gmail.com>
2015-09-19 14:24:05 -0400

    Remove comments

--- jobs/webcam.rb
+++ jobs/webcam.rb
@@ -14,7 +14,6 @@ require 'mechanize'
 @url4 = "http://traffic.ottawa.ca/map/camera?id=33"
 
 @a = Mechanize.new 
-#get cookie
 @a.get(@first_url)
 
 SCHEDULER.every '5s', first_in: 0 do |job|
@@ -22,69 +21,43 @@ SCHEDULER.every '5s', first_in: 0 do |job|
   get_camera(@url2, 'cam2')
   get_camera(@url3, 'cam3')
   get_camera(@url4, 'cam4')
-  # Increment a counter.
 end
 
 def get_camera(url, cam)
-  #get frame 0
   @a.get(url).save "public/#{cam}-tmp1.jpg"
-  #send_event for saved tmp file
   send_img("#{cam}-tmp1.jpg", cam)
   sleep(15.0/30.0)
   File.delete("public/#{cam}-tmp1.jpg")
   sleep(2)
-  #get frame 1
-  #do something with it, save?
   @a.get(url).save "public/#{cam}-tmp2.jpg"
-  #send_event for saved temp1 file
   send_img("#{cam}-tmp2.jpg", cam)
   sleep(15.0/30.0)
   File.delete("public/#{cam}-tmp2.jpg")
 end
 
-# SCHEDULER.every '15s', first_in: 0 do |job|
-#   send_event('cam1', image: "background-image:url(#{file_jpg})")
-#   sleep(1.0/30.0)
 def send_img(filename, cam)
   send_event(cam, image: "background-image:url(#{filename})")
 end
 
-
-
 END { 
-  File.delete("public/cam1-tmp1.jpg") if File.exist?("public/cam1-tmp1.jpg")
-  File.delete("public/cam1-tmp2.jpg") if File.exist?("public/cam1-tmp2.jpg")
+  FILE1a = "public/cam1-tmp1.jpg"
+  FILE1b = "public/cam1-tmp2.jpg"
+  FILE2a = "public/cam2-tmp1.jpg"
+  FILE2b = "public/cam2-tmp2.jpg"
+  FILE3a = "public/cam3-tmp1.jpg"
+  FILE3b = "public/cam3-tmp2.jpg"
+  FILE4a = "public/cam4-tmp1.jpg"
+  FILE4b = "public/cam4-tmp2.jpg"
 
-  File.delete("public/cam2-tmp1.jpg") if File.exist?("public/cam2-tmp1.jpg")
-  File.delete("public/cam2-tmp2.jpg") if File.exist?("public/cam2-tmp2.jpg")
+  File.delete(FILE1a) if File.exist?(FILE1a)
+  File.delete(FILE1b) if File.exist?(FILE1b)
 
-  File.delete("public/cam3-tmp1.jpg") if File.exist?("public/cam3-tmp1.jpg")
-  File.delete("public/cam3-tmp2.jpg") if File.exist?("public/cam3-tmp2.jpg")
+  File.delete(FILE2a) if File.exist?(FILE2a)
+  File.delete(FILE2b) if File.exist?(FILE2b)
 
-  File.delete("public/cam4-tmp1.jpg") if File.exist?("public/cam4-tmp1.jpg")
-  File.delete("public/cam4-tmp2.jpg") if File.exist?("public/cam4-tmp2.jpg")
+  File.delete(FILE3a) if File.exist?(FILE3a)
+  File.delete(FILE3b) if File.exist?(FILE3b)
+
+  File.delete(FILE4a) if File.exist?(FILE4a)
+  File.delete(FILE4b) if File.exist?(FILE4b)
 }
-
-
-#   send_event('cam2', image: "background-image:url(#{@first_url})")
-#   sleep(1.0/30.0)
-#   send_event('cam3', image: "background-image:url(#{@first_url})")
-#   sleep(1.0/30.0)
-#   send_event('cam4', image: "background-image:url(#{@first_url})")
-#   sleep(1.0/30.0)
-#   send_event('cam1', image: "background-image:url(#{@url1})")
-#   sleep(1.0/30.0)
-#   send_event('cam2', image: "background-image:url(#{url2})")
-#   sleep(1.0/30.0)
-#   send_event('cam3', image: "background-image:url(#{url3})")
-#   sleep(1.0/30.0)
-#   send_event('cam4', image: "background-image:url(#{url4})")
-#   sleep(1.0/2.0)
-#   send_event('cam1', image: "background-image:url(#{@url1})")
-#   sleep(1.0/2.0)
-#   send_event('cam2', image: "background-image:url(#{url2})")
-#   sleep(1.0/2.0)
-#   send_event('cam3', image: "background-image:url(#{url3})")
-#   sleep(1.0/2.0)
-#   send_event('cam4', image: "background-image:url(#{url4})")
-# end
