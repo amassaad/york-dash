2d6c95d96baf67005c3514f7f7622ae69112622d
Alex Massaad <alex.massaad@shopify.com>
2015-10-02 19:56:33 -0400

    Update webacam job to test faster update

--- jobs/webcam.rb
+++ jobs/webcam.rb
@@ -16,7 +16,7 @@ require 'mechanize'
 @a = Mechanize.new
 @a.get(@first_url)
 
-SCHEDULER.every '6s', first_in: 0 do |job|
+SCHEDULER.every '5s', first_in: 0 do |job|
   t1 = Thread.new{get_camera(@url1, 'cam1')}
   t2 = Thread.new{get_camera(@url2, 'cam2')}
   t3 = Thread.new{get_camera(@url3, 'cam3')}
@@ -25,12 +25,14 @@ end
 
 def get_camera(url, cam)
   @a.get(url).save "public/#{cam}-tmp1.jpg"
+  sleep(1)
   send_img("#{cam}-tmp1.jpg", cam)
   sleep(1)
-  File.delete("public/#{cam}-tmp1.jpg")
   @a.get(url).save "public/#{cam}-tmp2.jpg"
+  sleep(1)
   send_img("#{cam}-tmp2.jpg", cam)
   sleep(1)
+  File.delete("public/#{cam}-tmp1.jpg")
   File.delete("public/#{cam}-tmp2.jpg")
 end
 
